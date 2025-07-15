2460781962efe17c0760750712d6f1dd571101ef
Nickdvlp <117764824+Nickdvlp@users.noreply.github.com>
2025-07-15 10:05:51 +0530

    Update index.ts

--- backend/user service/src/index.ts
+++ backend/user service/src/index.ts
@@ -5,7 +5,7 @@ import userRoutes from "./route.js";
 import cors from "cors";
 
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
